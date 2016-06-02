e0dc2d48491e30217fe74d06da961a79c566a077
sghanti <santosh_ghanti@intuit.com>
2016-06-02 16:47:40 +0530

    Added Sample Project to Github

---
+++ GitSample/GitSample.cpp
@@ -0,0 +1,13 @@
+// GitSample.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <conio.h>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	std::cout << "My first git project" << std::endl;
+	_getch();
+}
+
